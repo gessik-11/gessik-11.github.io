6e85049c04b4e55a77a364b621f53d3f0b80ba95
gessik-11 <55445168+gessik-11@users.noreply.github.com>
2019-11-13 14:35:24 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -7,7 +7,7 @@ db.enablePersistence()
         }
     });
 
-db.collection('sobremesas').onSnapshot(snapshot => {
+db.collection('doces').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
             renderRecipe(change.doc.data(), change.doc.id);
